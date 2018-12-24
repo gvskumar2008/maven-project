84c449941590bb08cfa88cd42c29afeb002967f3
gvskumar2008 <42120966+gvskumar2008@users.noreply.github.com>
2018-12-24 10:05:52 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-  *@param name of person
-  *@return greeting with string
+  * @param someone name of person
+  * @return greeting with string
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
